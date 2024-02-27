fb7c757e1c8fc7a254bba5ab43edbfc102b63bf7
Bill Sideris <bill88t@feline.gr>
2024-02-28 01:31:08 +0200

    Fix strap

--- files/strap.py
+++ files/strap.py
@@ -2,5 +2,3 @@ for i in ["neofetch.lja", "neofetch.py"]:
     shutil.copy(i, path.join(root, "bin", i))
 
 shutil.copy("neofetch.man", path.join(root, "usr/share/man", "neofetch.man"))
-
-be.api.setvar("return", "0")
