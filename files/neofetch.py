f31912bb54c06e46ac481e57e9d7f022ef7ec390
Bill Sideris <bill88t@feline.gr>
2024-05-28 17:32:09 +0300

    Update neofetch

--- files/neofetch.py
+++ files/neofetch.py
@@ -26,6 +26,8 @@ vr(
     ),
 )
 
+vr("size", term.detect_size(3))
+vr("lomg", vr("size")[0] > 50)
 vr("time", int(pv[0]["uptimee"] + time.monotonic()))
 vr("ustr", "")
 vr("hr", vr("time") // 3600)  # Take out the hours
@@ -34,41 +36,56 @@ vr("min", vr("time") // 60)  # Take out the minutes
 vrm("time", vr("min") * 60)
 if vr("hr") > 0:
     vrp("ustr", str(vr("hr")) + " hours, ")
-if vr("min") > 0:
+if vr("min") > 0 and (vr("lomg") or not vr("hr")):
     vrp("ustr", str(vr("min")) + " minutes, ")
-if vr("time") > 0:
+if vr("time") > 0 and (vr("lomg") or not (vr("hr") or vr("min"))):
     vrp("ustr", str(vr("time")) + " seconds")
 else:
     vr("ustr", vr("ustr")[:-2])
 
-vr("cpul", str(platform))
-vrp("cpul", f" (1) @ {trunc(cpu.frequency / 1000000)}Mhz")
+if vr("lomg"):
+    vr("cpul", str(platform))
+    vrp("cpul", f" (1) @ {trunc(cpu.frequency / 1000000)}Mhz")
 
-vr("size", term.detect_size(3))
-
-vr("sep", 32)  # where do we begin collumn #2
-vr(
-    "logo",
-    [
-        f"{colors.be_main}     ...     ..{colors.endc}",
-        f'{colors.be_main}  .=*8888x <"?88h.     4{colors.endc}',
-        f"{colors.be_main} X>  '8888H> '8888{colors.endc}",
-        f"{colors.be_main}'88h. `8888   8888        .u{colors.endc}",
-        f"{colors.be_main}'8888 '8888    \"88>    ud8888.{colors.endc}",
-        f"{colors.be_main} `888 '8888.xH888x.  :888'8888.{colors.endc}",
-        f'{colors.be_main}   X" :88*~  `*8888> d888 \'88%"{colors.endc}',
-        f'{colors.be_main} ~"   !"`      "888> 8888.+"{colors.endc}',
-        f"{colors.be_main}  .H8888h.      ?88  8888L{colors.endc}",
-        f"{colors.be_main} :\"^\"88888h.    '!   '8888c. .+{colors.endc}",
-        f'{colors.be_main} ^    "88888hx.+"     "88888%{colors.endc}',
-        f'{colors.be_main}        ^"**""          "YP\'{colors.endc}',
-    ],
-)
+if vr("lomg"):
+    vr("sep", 32)  # where do we begin collumn #2
+    vr(
+        "logo",
+        [
+            f"{colors.be_main}     ...     ..{colors.endc}",
+            f'{colors.be_main}  .=*8888x <"?88h.     4{colors.endc}',
+            f"{colors.be_main} X>  '8888H> '8888{colors.endc}",
+            f"{colors.be_main}'88h. `8888   8888        .u{colors.endc}",
+            f"{colors.be_main}'8888 '8888    \"88>    ud8888.{colors.endc}",
+            f"{colors.be_main} `888 '8888.xH888x.  :888'8888.{colors.endc}",
+            f'{colors.be_main}   X" :88*~  `*8888> d888 \'88%"{colors.endc}',
+            f'{colors.be_main} ~"   !"`      "888> 8888.+"{colors.endc}',
+            f"{colors.be_main}  .H8888h.      ?88  8888L{colors.endc}",
+            f"{colors.be_main} :\"^\"88888h.    '!   '8888c. .+{colors.endc}",
+            f'{colors.be_main} ^    "88888hx.+"     "88888%{colors.endc}',
+            f'{colors.be_main}        ^"**""          "YP\'{colors.endc}',
+        ],
+    )
+    vr("ansi", True)
+else:
+    vr("sep", 14)
+    vr(
+        "logo",
+        [
+            ",-----------,",
+            "| 4    9.01 |",
+            "|           |",
+            "|           |",
+            "| BERYLLIUM |",
+            "'-----------'",
+        ],
+    )
+    vr("ansi", False)
 vr(
     "tex",
     [
         (
-            colors.yellow_t
+            (colors.yellow_t if vr("ansi") else "")
             + be.based.system_vars["USER"]
             + "@"
             + be.based.system_vars["HOSTNAME"]
@@ -76,18 +93,18 @@ vr(
         ),
         "---------",
         (
-            colors.yellow_t
+            (colors.yellow_t if vr("ansi") else "")
             + "OS"
             + colors.endc
             + ": "
-            + colors.be_main
+            + (colors.be_main if vr("ansi") else "")
             + "Beryllium "
             + be.based.system_vars["VERSION"]
             + colors.endc
         ),
         (colors.yellow_t + "Host" + colors.endc + ": " + be.based.system_vars["BOARD"]),
         (
-            colors.yellow_t
+            (colors.yellow_t if vr("ansi") else "")
             + "CircuitPython"
             + colors.endc
             + ": "
@@ -98,35 +115,48 @@ vr(
         + str(len(listdir(pv[0]["root"] + "/etc/jpkg/Installed")))
         + " (jpkg)",
         "{}Resolution:{} {}x{}".format(
-            colors.yellow_t, colors.endc, vr("size")[1], vr("size")[0]
+            colors.yellow_t if vr("ansi") else "", colors.endc, vr("size")[1], vr("size")[0]
         ),
-        "{}Terminal{}: {}".format(
-            colors.yellow_t, colors.endc, pv[0]["console_active"]
-        ),
-        "{}CPU{}: {}".format(colors.yellow_t, colors.endc, vr("cpul")),
-        "{}System Memory{}: {}".format(colors.yellow_t, colors.endc, vr("raml")),
     ],
 )
-
+if vr("lomg"):
+    vrp(
+        "tex",
+        [
+            "{}CPU{}: {}".format(colors.yellow_t if vr("ansi") else "", colors.endc, vr("cpul")),
+            "{}Terminal{}: {}".format(
+                colors.yellow_t if vr("ansi") else "", colors.endc, pv[0]["console_active"]
+            ),
+        ],
+    )
 vrp(
     "tex",
     [
-        "",
-        (
-            f"{colors.black_t}███{colors.endc}{colors.red_t}███{colors.endc}"
-            + f"{colors.green_t}███{colors.endc}{colors.yellow_t}███{colors.endc}"
-            + f"{colors.blue_t}███{colors.endc}{colors.magenta_t}███{colors.endc}"
-            + f"{colors.cyan_t}███{colors.endc}{colors.white_t}███{colors.endc}"
-        ),
-        (
-            f"{colors.black_t}███{colors.endc}"
-            + f"{colors.red_t}███{colors.endc}{colors.green_t}███{colors.endc}"
-            + f"{colors.yellow_t}███{colors.endc}{colors.blue_t}███{colors.endc}"
-            + f"{colors.magenta_t}███{colors.endc}{colors.cyan_t}███{colors.endc}"
-            + f"{colors.white_t}███{colors.endc}"
-        ),
+        "{}{}Memory{}: {}".format(colors.yellow_t if vr("ansi") else "", "System " if vr("lomg") else "", colors.endc, vr("raml")),
     ],
 )
+
+if vr("ansi"):
+    vrp(
+        "tex",
+        [
+            "",
+            (
+                f"{colors.black_t}███{colors.endc}{colors.red_t}███{colors.endc}"
+                + f"{colors.green_t}███{colors.endc}{colors.yellow_t}███{colors.endc}"
+                + f"{colors.blue_t}███{colors.endc}{colors.magenta_t}███{colors.endc}"
+                + f"{colors.cyan_t}███{colors.endc}{colors.white_t}███{colors.endc}"
+            ),
+            (
+                f"{colors.black_t}███{colors.endc}"
+                + f"{colors.red_t}███{colors.endc}{colors.green_t}███{colors.endc}"
+                + f"{colors.yellow_t}███{colors.endc}{colors.blue_t}███{colors.endc}"
+                + f"{colors.magenta_t}███{colors.endc}{colors.cyan_t}███{colors.endc}"
+                + f"{colors.white_t}███{colors.endc}"
+            ),
+        ],
+    )
+
 for pv[get_pid()]["i"] in range(0, max(len(vr("logo")), len(vr("tex")))):
     try:
         term.nwrite(
